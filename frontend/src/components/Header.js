5da37ae74f599b4b73ceac5b436c26cd1c515077
808vita <its808vita@gmail.com>
2022-10-19 18:58:37 +0530

    frontend auth0 setup | test auth

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,20 +5,35 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { authenticatedUser, verifiedUser, resetUserAuths } from "../redux/user";
 
+import { useAuth0 } from "@auth0/auth0-react";
 const Header = () => {
   const { isAuth, isVerified } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   //testing user slice , dispatch and selector
   //modify next round
+
+  const { loginWithPopup, logout, user, isAuthenticated } = useAuth0();
   return (
-    <div>
-      <h1>Header</h1>
-      auth: <p>{String(isAuth)}</p>
-      verified: <p>{String(isVerified)}</p>
-      <button onClick={() => dispatch(authenticatedUser())}>set auth</button>
-      <button onClick={() => dispatch(verifiedUser())}>set verified</button>
-      <button onClick={() => dispatch(resetUserAuths())}>reset user</button>
-    </div>
+    <>
+      <div>
+        <h1>checking auth0</h1>
+        isAuthenticated: <p>{String(isAuthenticated)}</p>
+        <pre style={{ textAlign: "start" }}>
+          {JSON.stringify(user, null, 2)}
+        </pre>
+        <button onClick={loginWithPopup}>login pop</button>
+        <button onClick={logout}>logout</button>
+      </div>
+
+      <div>
+        <h1>Header</h1>
+        auth: <p>{String(isAuth)}</p>
+        verified: <p>{String(isVerified)}</p>
+        <button onClick={() => dispatch(authenticatedUser())}>set auth</button>
+        <button onClick={() => dispatch(verifiedUser())}>set verified</button>
+        <button onClick={() => dispatch(resetUserAuths())}>reset user</button>
+      </div>
+    </>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain="dev-r-n4l5rq.us.auth0.com"
+      clientId="gPO2Iwuo8b6Z2IbGKPsWAvd2sk6nI4KB"
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>
 );
