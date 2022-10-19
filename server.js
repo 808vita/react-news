0c04b07a07e0dd17e577b078b6e52698c3b6756d
808vita <its808vita@gmail.com>
2022-10-20 05:28:16 +0530

    cors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const articleRoutes = require("./routes/articleRoutes");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 const port = 4000;
 
