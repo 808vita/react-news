680ebe7e6a0db597c823555af8e825c8791f4960
808vita <its808vita@gmail.com>
2022-10-20 01:15:14 +0530

    Full article starter

--- frontend/src/pages/FullArticle.js
+++ frontend/src/pages/FullArticle.js
@@ -1,13 +1,29 @@
 //requires protection
 //detailed article
 import React from "react";
-
+import { useSelector } from "react-redux";
 const FullArticle = () => {
-  return (
-    <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
-      FullArticle
-    </div>
-  );
+  const { isVerified, isAuth } = useSelector((state) => state.user);
+
+  if (!isAuth) {
+    return (
+      <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
+        please Login
+      </div>
+    );
+  } else if (isAuth && !isVerified) {
+    return (
+      <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
+        auth but not verified
+      </div>
+    );
+  } else if (isAuth && isVerified) {
+    return (
+      <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
+        valid auth with verification
+      </div>
+    );
+  }
 };
 
 export default FullArticle;
